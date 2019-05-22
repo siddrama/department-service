05d2eab268b968bdf787e99dd2c40af4710a19c0
siddrama <sidda>
2019-05-22 09:54:22 -0400

    [Cloud]  Added Department to clould

--- src/main/java/pl/piomin/services/department/DepartmentApplication.java
+++ src/main/java/pl/piomin/services/department/DepartmentApplication.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
@@ -20,6 +21,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableFeignClients
 @EnableMongoRepositories
 @EnableSwagger2
+@EnableEurekaClient
 public class DepartmentApplication {
 
 	@Value("${VERSION}")
