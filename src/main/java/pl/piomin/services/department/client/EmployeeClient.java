6af913c7643c3f843b31f32a97ee9ecff6484d18
siddrama <sidda>
2019-05-23 00:21:13 -0400

    [Cloud] Added Department to clould

--- src/main/java/pl/piomin/services/department/client/EmployeeClient.java
+++ src/main/java/pl/piomin/services/department/client/EmployeeClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import pl.piomin.services.department.model.Employee;
 
-@FeignClient(name = "EMPLOYEE", url = "${microservices.employee.url}")
+@FeignClient("employee")
 public interface EmployeeClient {
 
 	@GetMapping("/employee/department/{departmentId}")
