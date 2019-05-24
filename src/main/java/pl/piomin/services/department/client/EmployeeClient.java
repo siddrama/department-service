baef5c4f5f946bdc319e2d83c3e1b1dcf34d491f
siddrama <sidda>
2019-05-24 14:03:05 -0400

    [Cloud] Added Department to clould Update1

--- src/main/java/pl/piomin/services/department/client/EmployeeClient.java
+++ src/main/java/pl/piomin/services/department/client/EmployeeClient.java
@@ -11,7 +11,7 @@ import pl.piomin.services.department.model.Employee;
 @FeignClient(name="employee")
 public interface EmployeeClient {
 	
-	@GetMapping("/employee/department/{departmentId}")
+	@GetMapping("/department/{departmentId}")
 	List<Employee> findByDepartment(@PathVariable("departmentId") String departmentId);
 	
 }
